c6179c4718f1174b8537e1ce1f9b9f356b21a3d9
samuele <samuelebompani@gmail.com>
2022-06-21 21:10:04 +0200

    showing result

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/style.css">
+        <title>Pendolino test</title>
     </head>
     <body>
         <?php include('components/navbar.php')?>
         <div class="container">
             <form action="result.php" method="post">
                 <div class="q-container">
-                    <div><h2>Il traffico e l'arbre magique</h2>
+                    <div><h2>Il traffico e l'arbre magique - PND#34</h2>
                         <div class="q-body">A Roma c’è molto traffico, quasi come a Los Angeles. Puoi risolvere questo problema per sempre, ma non solo: in qualunque posto arrivi trovi sempre parcheggio, non solo a Roma. In cambio, per tutta la vita e in qualunque contesto devi portare al collo un arbre magique. Se le persone ti chiedono e/o ti prendono in giro, devi far finta di non averlo. Ovviamente l’arbre magique rimane su anche durante i rapporti sessuali.</div>
                     </div>
                     <div class="q-option">
---
+++ result.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+        <title>Pendolino test</title>
+    </head>
+    <body>
+        <?php include('components/navbar.php')?>
+        <?php 
+            $result = array(0,0,0);
+            $names = array("Marco", "Dario", "Emanuele");
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                foreach($_POST as $ss => $s) {
+                                    $index = (int)$s;
+                                    $result[$index] += 1;
+                                    print($result[0]);
+                                    print($result[1]);
+                                    print($result[2]);
+                                }
+                            }
+            $max = 0;
+            foreach($result as $i => $v) {
+                if($v > $result[$max]) {
+                    $max = $i;
+                }
+            }?>
+        <div class="container">
+            <div style="text-align: center;">
+                <h2>Se fossi un conduttore di Pendolino, saresti <?php print($names[$max])?>!</h2>
+            </div>
+
+            <div style="text-align: center; margin-top: 2%;">
+                <a href="/"><button class="submit-btn" type="submit">Torna al test</button></a>
+            </div>
+        </div>
+        <?php include("components/footer.php"); ?>
+    </body>
+</html>
