88d0a468dd3a752d5ba033aa30ac92753ba33616
samuele <samuelebompani@gmail.com>
2022-06-22 19:18:56 +0200

    typo and adjustments

--- components/navbar.php
+++ components/navbar.php
@@ -1,8 +1,8 @@
 <div class="title-nav">
-    <h1>Pendolino test</h1>
-    
+    <h1><a href="./" style="text-decoration: none; color: black;">Pendolino test</a></h1>
+</div>
+<div style="text-align: center;">
     <a href="./about">
         <button class="nav-btn">About</button>
     </a>
-    
 </div>
