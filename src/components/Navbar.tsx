1cb266ff7e65bbfa9a17bfc980e4661ae2bc7aa5
samuele <samuelebompani@gmail.com>
2022-06-29 14:41:21 +0200

    whitebox

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar(): JSX.Element {
     return <>
-        <div className="text-center text-4xl font-bold py-5 border-b-4 border-solid border-black">
+        <div className="text-center text-5xl font-bold py-8 border-b-4 border-solid border-black">
             <Link to="./">Pendolino test</Link>
         </div>
-        <div className="text-center">
+        <div className="text-center font-bold">
             <Link to="./about">
                 <button className="my-2">About</button>
             </Link>
--- src/routes/Home.route.tsx
+++ src/routes/Home.route.tsx
@@ -1,3 +1,10 @@
+import WhiteBox from "../components/WhiteBox";
+
 export default function HomeRoute(): JSX.Element {
-    return <></>
+    return <>
+        <div className="container">
+            <WhiteBox title="Helo" body={"Domanda"} />
+        </div>
+        
+    </>
 }
